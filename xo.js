e67dc5e3aaace368f8c0e68124ccb47602891163
Vishnu Vardhan <.gitbalavishnuvardhan2@gmail.com>
2024-08-15 21:45:51 +0530

    XO game

---
+++ xo.js
@@ -0,0 +1,100 @@
+let click=document.getElementsByTagName('img')
+let random=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[0].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[0].src='./image/tic2.png';
+    }
+}
+
+let random1=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[1].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[1].src='./image/tic2.png';
+    }
+}
+
+let random2=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[2].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[2].src='./image/tic2.png';
+    }
+}
+
+let random3=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[3].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[3].src='./image/tic2.png';
+    }
+}
+
+let random4=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[4].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[4].src='./image/tic2.png';
+    }
+}
+
+let random5=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[5].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[5].src='./image/tic2.png';
+    }
+}
+
+let random6=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[6].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[6].src='./image/tic2.png';
+    }
+}
+
+let random7=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[7].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[7].src='./image/tic2.png';
+    }
+}
+
+let random8=()=>{
+    let val=prompt("Enter any X or O")
+    if(val=='X'){
+        click[8].src='./image/tic1.jpg';
+    }
+    else if(val=='O'){
+        click[8].src='./image/tic2.png';
+    }
+}
+
+// let random9=()=>{
+//     let val=prompt("Enter any X or O")
+//     if(val=='X'){
+//         click[9].src='./image/tic1.jpg';
+//     }
+//     else{
+//         click[9].src='./image/tic2.png';
+//     }
+// }
